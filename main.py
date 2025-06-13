fbc0c9dce6ad2162c821199a3a5f0942a0b97fa6
Blank <chris.tong.muc@gmail.com>
2025-06-13 17:55:36 +0200

    can actually now call functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info, get_file_content, run_python_file, write_file
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -89,6 +90,46 @@ available_functions = types.Tool(
     ]
 )
 
+def call_function(function_call_part, verbose=False):
+	if verbose == True:
+		print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+	else:
+		print(f" - Calling function: {function_call_part.name}")
+
+	function_map = {
+		"get_files_info": get_files_info,
+		"get_file_content": get_file_content,
+		"run_python_file": run_python_file,
+		"write_file": write_file,
+		}
+
+	if function_call_part.name not in function_map:
+                return types.Content(
+                        role="tool",
+                        parts=[
+                                types.Part.from_function_response(
+                                        name=function_call_part.name,
+                                        response={"error": f"Unknown function: {function_call_part.name}"},
+                                )
+                        ],
+                )
+
+	else:
+		actual_function = function_map[function_call_part.name]
+		args = function_call_part.args.copy()
+		args["working_directory"] = "./calculator"
+		result = actual_function(**args)
+		return types.Content(
+			role="tool",
+			parts=[
+				types.Part.from_function_response(
+					name=function_call_part.name,
+					response={"result": result},
+				)
+			],
+		)
+
+
 if "--verbose" in sys.argv:
 	is_verbose = True
 if len(sys.argv) > 1:
@@ -96,9 +137,12 @@ if len(sys.argv) > 1:
 	messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 	response = client.models.generate_content(model =  "gemini-1.5-flash-latest", contents = messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),)
 	if response.function_calls:
-		print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
-		print(response.text)
-	else: print(response.text)
+		function_call_result = call_function(response.function_calls[0], verbose=is_verbose)
+		if function_call_result.parts[0].function_response.response:
+			if is_verbose:
+				print(f"-> {function_call_result.parts[0].function_response.response}")
+		else:
+			raise Exception("Function call didn't return expected response structure")
 	if is_verbose == True:
 		print(f"User prompt: {user_prompt}")
 		print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
