c63f966b46d757b9746c567c5db66fbf93b2f0bb
Blank <chris.tong.muc@gmail.com>
2025-06-13 10:47:06 +0200

    adding starting prompt

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 is_verbose = False
+system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
 
 if "--verbose" in sys.argv:
-	is_verbose = True 
+	is_verbose = True
 if len(sys.argv) > 1:
 	user_prompt = sys.argv[1]
 	messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-	result = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages)
-	print(result.text)
+	response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = messages, config=types.GenerateContentConfig(system_instruction=system_prompt),)
+	print(response.text)
 	if is_verbose == True:
 		print(f"User prompt: {user_prompt}")
-		print(f"Prompt tokens: {result.usage_metadata.prompt_token_count}")
-		print(f"Response tokens: {result.usage_metadata.candidates_token_count}")
+		print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+		print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 else: sys.exit(1)
 
